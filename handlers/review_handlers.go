18060b5997a3a69d79c8983645c9d02a8d9c1c2c
faiq <faiq@voltras.co.id>
2025-03-17 14:08:21 +0700

    beres beres review handler

--- handlers/review_handlers.go
+++ handlers/review_handlers.go
@@ -2,14 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
-	"iv_project/dto"
 	review_dto "iv_project/dto/review"
 	"iv_project/models"
 	"iv_project/repositories"
 	"net/http"
 	"strconv"
 
-	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 )
 
@@ -32,84 +30,91 @@ func ConvertToReviewResponse(review *models.Review) review_dto.ReviewResponse {
 func (h *reviewHandlers) CreateReview(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	request := new(review_dto.ReviewRequest)
+	var request review_dto.CreateReviewRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()})
+		ErrorResponse(w, http.StatusBadRequest, "Invalid request format. Please check your input.")
 		return
 	}
 
-	validation := validator.New()
-	err := validation.Struct(request)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()})
-		return
-	}
-
-	review := models.Review{
+	review := &models.Review{
 		UserID:            request.UserID,
 		InvitationThemeID: request.InvitationThemeID,
 		Star:              request.Star,
 		Comment:           request.Comment,
 	}
 
-	err = h.ReviewRepositories.CreateReview(review)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()})
+	if err := h.ReviewRepositories.CreateReview(review); err != nil {
+		ErrorResponse(w, http.StatusInternalServerError, "An error occurred while create the review. Please try again.")
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(dto.SuccessResult{Code: http.StatusOK, Data: "Review created successfully"})
+	SuccessResponse(w, http.StatusCreated, "Review successfully created", ConvertToReviewResponse(review))
 }
 
 func (h *reviewHandlers) GetReviewByID(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	id, _ := strconv.Atoi(mux.Vars(r)["id"])
-	review, err := h.ReviewRepositories.GetReviewByID(uint(id))
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(dto.ErrorResult{Code: http.StatusNotFound, Message: "Review not found"})
+		ErrorResponse(w, http.StatusBadRequest, "Invalid ID format. Please use a number.")
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(dto.SuccessResult{Code: http.StatusOK, Data: review})
+	review, err := h.ReviewRepositories.GetReviewByID(uint(id))
+	if err != nil {
+		ErrorResponse(w, http.StatusNotFound, "Review not found with the given ID.")
+		return
+	}
+
+	SuccessResponse(w, http.StatusOK, "Review found successfully", ConvertToReviewResponse(review))
 }
 
 func (h *reviewHandlers) GetReviewsByInvitationThemeID(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	id, _ := strconv.Atoi(mux.Vars(r)["invitationThemeId"])
-	reviews, err := h.ReviewRepositories.GetReviewsByInvitationThemeID(uint(id))
+	id, err := strconv.Atoi(mux.Vars(r)["invitationThemeId"])
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()})
+		ErrorResponse(w, http.StatusBadRequest, "Invalid ID format. Please use a number.")
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(dto.SuccessResult{Code: http.StatusOK, Data: reviews})
+	reviews, err := h.ReviewRepositories.GetReviewsByInvitationThemeID(uint(id))
+	if err != nil {
+		ErrorResponse(w, http.StatusInternalServerError, "An error occurred while retrieving reviews. Please try again.")
+		return
+	}
+
+	if len(reviews) == 0 {
+		SuccessResponse(w, http.StatusOK, "No reviews available for this invitation theme", []review_dto.ReviewResponse{})
+		return
+	}
+
+	var response []review_dto.ReviewResponse
+	for _, review := range reviews {
+		response = append(response, ConvertToReviewResponse(&review))
+	}
+
+	SuccessResponse(w, http.StatusOK, "Reviews retrieved successfully", response)
 }
 
 func (h *reviewHandlers) UpdateReview(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	request := new(review_dto.ReviewRequest)
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()})
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		ErrorResponse(w, http.StatusBadRequest, "Invalid ID format. Please use a number.")
 		return
 	}
 
-	id, _ := strconv.Atoi(mux.Vars(r)["id"])
 	review, err := h.ReviewRepositories.GetReviewByID(uint(id))
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(dto.ErrorResult{Code: http.StatusNotFound, Message: "Review not found"})
+		ErrorResponse(w, http.StatusNotFound, "Review not found with the given ID.")
+		return
+	}
+
+	var request review_dto.UpdateReviewRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		ErrorResponse(w, http.StatusBadRequest, "Invalid request format. Please check your input.")
 		return
 	}
 
@@ -120,35 +125,27 @@ func (h *reviewHandlers) UpdateReview(w http.ResponseWriter, r *http.Request) {
 		review.Comment = request.Comment
 	}
 
-	err = h.ReviewRepositories.UpdateReview(review)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()})
+	if err := h.ReviewRepositories.UpdateReview(review); err != nil {
+		ErrorResponse(w, http.StatusInternalServerError, "An error occurred while updating the review. Please try again.")
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(dto.SuccessResult{Code: http.StatusOK, Data: "Review updated successfully"})
+	SuccessResponse(w, http.StatusOK, "Review successfully updated", ConvertToReviewResponse(review))
 }
 
 func (h *reviewHandlers) DeleteReview(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	id, _ := strconv.Atoi(mux.Vars(r)["id"])
-	review, err := h.ReviewRepositories.GetReviewByID(uint(id))
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(dto.ErrorResult{Code: http.StatusNotFound, Message: "Review not found"})
+		ErrorResponse(w, http.StatusBadRequest, "Invalid ID format. Please use a number.")
 		return
 	}
 
-	err = h.ReviewRepositories.DeleteReview(review)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()})
+	if err := h.ReviewRepositories.DeleteReview(uint(id)); err != nil {
+		ErrorResponse(w, http.StatusInternalServerError, "An error occurred while deleting the review. Please try again.")
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(dto.SuccessResult{Code: http.StatusOK, Data: "Review deleted successfully"})
+	SuccessResponse(w, http.StatusOK, "Review successfully deleted", nil)
 }
