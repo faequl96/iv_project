7bda0db2e5de2a3d39aa5757e1e3c1a3095f2e4f
faiq <faiq@voltras.co.id>
2025-03-19 12:16:42 +0700

    add user role enum

--- models/user.go
+++ models/user.go
@@ -6,8 +6,17 @@ import (
 	"gorm.io/plugin/soft_delete"
 )
 
+type UserRoleType string
+
+const (
+	UserRoleSuperAdmin UserRoleType = "super_admin"
+	UserRoleAdmin      UserRoleType = "admin"
+	UserRoleUser       UserRoleType = "user"
+)
+
 type User struct {
 	ID          string       `gorm:"primaryKey;size:36" json:"id"` // Firebase UID
+	Role        UserRoleType `gorm:"type:varchar(50);not null;default:'user'" json:"role"`
 	UserProfile *UserProfile `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"user_profile,omitempty"`
 	IVCoin      *IVCoin      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"iv_coin,omitempty"`
 
